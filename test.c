92bef76b33df37453ecb17b2ed7e534edb41f005
Felipe Ferreira da Silva <ferreiradaselva@protonmail.com>
2017-10-13 22:56:13 -0300

    Update test

--- test.c
+++ test.c
@@ -20,87 +20,137 @@ the following restrictions:
 
 #include <stdio.h>
 #include <float.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include "mathc.h"
 
 const float epsilon = FLT_EPSILON;
-const float epsilon_greater = FLT_EPSILON * 10.0f;
 
 struct cerror {
 	int32_t failed;
 	int32_t passed;
-	int32_t passed_with_greater_error_margin;
+	int32_t passed_with_e10;
+	int32_t passed_with_e100;
+	int32_t passed_with_e1000;
 };
 
 void printf_1f_test(struct cerror *error, char *msg, float e1, float r1)
 {
+	bool done = false;
 	printf("%s:\n\tExpected % .4f\n\t  Actual  % .4f\t", msg, e1, r1);
 	if (nearly_equal(e1, r1, epsilon)) {
 		error->passed = error->passed + 1;
+		done = true;
 		printf("~passed~\n\n");
-	} else {
-		/* Try with greater error margin */
-		if (nearly_equal(e1, r1, epsilon_greater)) {
-			error->passed_with_greater_error_margin = error->passed_with_greater_error_margin + 1;
-			printf("~passed with greater error margin~\n\n");
-		} else {
-			error->failed = error->failed + 1;
-			printf("~failed~\n\n");
-		}
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 10.0f)) {
+		error->passed_with_e10 = error->passed_with_e10 + 1;
+		done = true;
+		printf("~passed with epsilon * 10.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 100.0f)) {
+		error->passed_with_e100 = error->passed_with_e100 + 1;
+		done = true;
+		printf("~passed with epsilon * 100.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 1000.0f)) {
+		error->passed_with_e1000 = error->passed_with_e1000 + 1;
+		done = true;
+		printf("~passed with epsilon * 1000.0~\n\n");
+	}
+	if (!done) {
+		error->failed = error->failed + 1;
+		printf("~failed~\n\n");
 	}
 }
 
 void printf_2f_test(struct cerror *error, char *msg, float e1, float e2, float r1, float r2)
 {
+	bool done = false;
 	printf("%s:\n\tExpected % .4f, % .4f\n\t  Actual % .4f, % .4f\t", msg, e1, e2, r1, r2);
 	if (nearly_equal(e1, r1, epsilon) && nearly_equal(e2, r2, epsilon)) {
 		error->passed = error->passed + 1;
+		done = true;
 		printf("~passed~\n\n");
-	} else {
-		/* Try with greater error margin */
-		if (nearly_equal(e1, r1, epsilon_greater) && nearly_equal(e2, r2, epsilon_greater)) {
-			error->passed_with_greater_error_margin = error->passed_with_greater_error_margin + 1;
-			printf("~passed with greater error margin~\n\n");
-		} else {
-			error->failed = error->failed + 1;
-			printf("~failed~\n\n");
-		}
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 10.0f) && nearly_equal(e2, r2, epsilon * 10.0f)) {
+		error->passed_with_e10 = error->passed_with_e10 + 1;
+		done = true;
+		printf("~passed with epsilon * 10.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 100.0f) && nearly_equal(e2, r2, epsilon * 100.0f)) {
+		error->passed_with_e100 = error->passed_with_e100 + 1;
+		done = true;
+		printf("~passed with epsilon * 100.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 1000.0f) && nearly_equal(e2, r2, epsilon * 1000.0f)) {
+		error->passed_with_e1000 = error->passed_with_e1000 + 1;
+		done = true;
+		printf("~passed with epsilon * 1000.0~\n\n");
+	}
+	if (!done) {
+		error->failed = error->failed + 1;
+		printf("~failed~\n\n");
 	}
 }
 
 void printf_3f_test(struct cerror *error, char *msg, float e1, float e2, float e3, float r1, float r2, float r3)
 {
+	bool done = false;
 	printf("%s:\n\tExpected % .4f, % .4f, % .4f\n\t  Actual % .4f, % .4f, % .4f\t", msg, e1, e2, e3, r1, r2, r3);
 	if (nearly_equal(e1, r1, epsilon) && nearly_equal(e2, r2, epsilon) && nearly_equal(e3, r3, epsilon)) {
 		error->passed = error->passed + 1;
+		done = true;
 		printf("~passed~\n\n");
-	} else {
-		/* Try with greater error margin */
-		if (nearly_equal(e1, r1, epsilon_greater) && nearly_equal(e2, r2, epsilon_greater) && nearly_equal(e3, r3, epsilon_greater)) {
-			error->passed_with_greater_error_margin = error->passed_with_greater_error_margin + 1;
-			printf("~passed with greater error margin~\n\n");
-		} else {
-			error->failed = error->failed + 1;
-			printf("~failed~\n\n");
-		}
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 10.0f) && nearly_equal(e2, r2, epsilon * 10.0f) && nearly_equal(e3, r3, epsilon * 10.0f)) {
+		error->passed_with_e10 = error->passed_with_e10 + 1;
+		done = true;
+		printf("~passed with epsilon * 10.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 100.0f) && nearly_equal(e2, r2, epsilon * 100.0f) && nearly_equal(e2, r2, epsilon * 100.0f)) {
+		error->passed_with_e100 = error->passed_with_e100 + 1;
+		done = true;
+		printf("~passed with epsilon * 100.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 1000.0f) && nearly_equal(e2, r2, epsilon * 1000.0f) && nearly_equal(e2, r2, epsilon * 1000.0f)) {
+		error->passed_with_e1000 = error->passed_with_e1000 + 1;
+		done = true;
+		printf("~passed with epsilon * 1000.0~\n\n");
+	}
+	if (!done) {
+		error->failed = error->failed + 1;
+		printf("~failed~\n\n");
 	}
 }
 
 void printf_4f_test(struct cerror *error, char *msg, float e1, float e2, float e3, float e4, float r1, float r2, float r3, float r4)
 {
-	printf("%s:\n\tExpected % .4f, % .4f, % .4f, % .4f\n\t  Actual % .4f, % .4f, % .4f, % .4f\t", msg, e1, e2, e3, e4, r1, r2, r3, r4);
-	if (nearly_equal(e1, r1, epsilon) && nearly_equal(e2, r2, epsilon) && nearly_equal(e3, r3, epsilon)) {
+	bool done = false;
+	printf("%s:\n\tExpected % .9f, % .9f, % .9f, % .9f\n\t  Actual % .9f, % .9f, % .9f, % .9f\t", msg, e1, e2, e3, e4, r1, r2, r3, r4);
+	if (nearly_equal(e1, r1, epsilon) && nearly_equal(e2, r2, epsilon) && nearly_equal(e3, r3, epsilon) && nearly_equal(e3, r3, epsilon)) {
 		error->passed = error->passed + 1;
+		done = true;
 		printf("~passed~\n\n");
-	} else {
-		/* Try with greater error margin */
-		if (nearly_equal(e1, r1, epsilon_greater) && nearly_equal(e2, r2, epsilon_greater) && nearly_equal(e3, r3, epsilon_greater) && nearly_equal(e4, r4, epsilon_greater)) {
-			error->passed_with_greater_error_margin = error->passed_with_greater_error_margin + 1;
-			printf("~passed with greater error margin~\n\n");
-		} else {
-			error->failed = error->failed + 1;
-			printf("~failed~\n\n");
-		}
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 10.0f) && nearly_equal(e2, r2, epsilon * 10.0f) && nearly_equal(e3, r3, epsilon * 10.0f) && nearly_equal(e3, r3, epsilon * 10.0f)) {
+		error->passed_with_e10 = error->passed_with_e10 + 1;
+		done = true;
+		printf("~passed with epsilon * 10.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 100.0f) && nearly_equal(e2, r2, epsilon * 100.0f) && nearly_equal(e3, r3, epsilon * 100.0f) && nearly_equal(e4, r4, epsilon * 100.0f)) {
+		error->passed_with_e100 = error->passed_with_e100 + 1;
+		done = true;
+		printf("~passed with epsilon * 100.0~\n\n");
+	}
+	if (!done && nearly_equal(e1, r1, epsilon * 1000.0f) && nearly_equal(e2, r2, epsilon * 1000.0f) && nearly_equal(e3, r3, epsilon * 1000.0f) && nearly_equal(e4, r4, epsilon * 1000.0f)) {
+		error->passed_with_e1000 = error->passed_with_e1000 + 1;
+		done = true;
+		printf("~passed with epsilon * 1000.0~\n\n");
+	}
+	if (!done) {
+		error->failed = error->failed + 1;
+		printf("~failed~\n\n");
 	}
 }
 
@@ -321,10 +371,15 @@ void quaternion_tests(struct cerror *error)
 	struct vec r;
 	float p;
 	printf("\n# Making tests with quaternions...\n");
-	a = to_quaternion(1.0f, 1.0f, 1.0f, 1.0f);
-	b = to_quaternion(1.0f, 1.0f, 1.0f, 1.0f);
-	r = quaternion_add(a, b);
-	printf_4f_test(error, "Add two quaternions", 2.0f, 2.0f, 2.0f, 2.0f, r.x, r.y, r.z, r.w);
+	a = to_quaternion(0.0f, 1.0f, 0.0f, 1.0f);
+	r = quaternion_normalize(a);
+	printf_4f_test(error, "Normalize quaternion", 0.707099974f, 0.0, 0.707099974f, 0.0f, r.w, r.x, r.y, r.z);
+	a = to_vector3(1.0f, 0.0f, 0.0f);
+	r = quaternion_from_axis_angle(a, M_PIF_2);
+	printf_4f_test(error, "Quaternion from axis-angle", 0.707099974f, 0.707099974f, 0.0f, 0.0f, r.w, r.x, r.y, r.z);
+	a = to_quaternion(0.7071f, 0.0f, 0.0f, 0.7071f);
+	r = quaternion_to_axis_angle(a);
+	printf_4f_test(error, "Quaternion to axis-angle", 1.0f, 0.0f, 0.0f, 1.570796371f, r.x, r.y, r.z, r.w);
 }
 
 void matrix_tests(struct cerror *error)
@@ -348,6 +403,8 @@ int main(int argc, char **args)
 	matrix_tests(&error);
 	printf("\nTotal of failed tests: %d\n", error.failed);
 	printf("Total of tests that passed: %d\n", error.passed);
-	printf("Total of tests that passed with greater error margin: %d\n", error.passed_with_greater_error_margin);
+	printf("Total of tests that passed with epsilon * 10.0: %d\n", error.passed_with_e10);
+	printf("Total of tests that passed with epsilon * 100.0: %d\n", error.passed_with_e100);
+	printf("Total of tests that passed with epsilon * 1000.0: %d\n", error.passed_with_e1000);
 	return error.failed;
 }
