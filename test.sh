5eb64f4d41f44c42a8a8920a6383aaaa81be55ac
Felipe Ferreira da Silva <ferreiradaselva@protonmail.com>
2018-04-08 17:07:09 -0300

    Update test script

--- test.sh
+++ test.sh
@@ -2,11 +2,15 @@ if [ -n $CC ]
 then
   if [ $CC=gcc ]
   then
-    gcc -std=c99 -Wall -Wextra -o test_gcc test.c mathc.c -I. -lm && chmod +x test_gcc
+    rm -f test_gcc
+    gcc -std=c99 -pedantic -Werror -Wall -Wextra -o test_gcc test.c mathc.c -lm
+    chmod +x test_gcc
     ./test_gcc
   elif [ $CC=clang ]
   then
-    clang test.c -include mathc.h -c mathc.c && clang -o test_clang test.o mathc.o -lm && chmod +x test_clang
+    rm -f test_clang
+    clang -std=c99 -pedantic -Werror -Wall -Wextra -o test_clang test.c mathc.c -lm
+    chmod +x test_clang
     ./test_clang
   else
     echo -e "Failed to build test: compiler (gcc or clang) not found."
