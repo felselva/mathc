95374dc7d18b29f605ebacf0885187aefefd8fc2
Eleftheria Chatziargyriou <ele.hatzy@gmail.com>
2019-02-13 23:45:04 +0200

    Add vec3 rotation

--- mathc.c
+++ mathc.c
@@ -1382,6 +1382,23 @@ mfloat_t *vec3_reflect(mfloat_t *result, mfloat_t *v0, mfloat_t *normal)
 	return result;
 }
 
+mfloat_t *vec3_rotate(mfloat_t *result, mfloat_t *v0, mfloat_t *ra, mfloat_t f)
+{
+	mfloat_t cs = MCOS(f);
+	mfloat_t sn = MSIN(f);
+	mfloat_t x = v0[0];
+	mfloat_t y = v0[1];
+	mfloat_t z = v0[2];
+	vec3_normalize(ra, ra);
+	mfloat_t rx = ra[0];
+	mfloat_t ry = ra[1];
+	mfloat_t rz = ra[2];
+	result[0] = x * (cs + rx * rx*(1 - cs)) + y * (rx*ry*(1 - cs) - rz * sn) + z * (rx*rz*(1 - cs) + ry * sn);
+	result[1] = x * (ry*rx*(1 - cs) + rz * sn) + y * (cs + ry * ry*(1 - cs)) + z * (ry*rz*(1 - cs) - rx * sn);
+	result[2] = x * (rz*rx*(1 - cs) - ry * sn) + y * (rz*ry*(1 - cs) + rx * sn) + z * (cs + rz * rz*(1 - cs));
+	return result;
+}
+
 mfloat_t *vec3_lerp(mfloat_t *result, mfloat_t *v0, mfloat_t *v1, mfloat_t f)
 {
 	result[0] = v0[0] + (v1[0] - v0[0]) * f;
